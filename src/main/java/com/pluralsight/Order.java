ca22edfe63f5438157a858446df0e4b28c524490
Ivan Tavarez <ivantavarez2@gmail.com>
2023-11-27 12:50:14 -0500

    Created Order class

--- src/main/java/com/pluralsight/Order.java
+++ src/main/java/com/pluralsight/Order.java
@@ -4,11 +4,11 @@ import java.util.List;
 public class Order {
     private int orderID;
     private String customerName;
-    private List<Sandwich> sandwiches;
+    private List<Sandwhich> sandwiches;
     private List<Drink> drinks;
-    private List<Chip> chips;
+    private List<Chips> chips;
 
-    public Order(int orderID, String customerName, List<Sandwich> sandwiches, List<Drink> drinks, List<Chip> chips) {
+    public Order(int orderID, String customerName, List<Sandwhich> sandwiches, List<Drink> drinks, List<Chips> chips) {
         this.orderID = orderID;
         this.customerName = customerName;
         this.sandwiches = sandwiches;
