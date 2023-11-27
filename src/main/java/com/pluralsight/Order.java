9b157aea92ba323624a10753a6af0ddc7fc4b81b
Ivan Tavarez <ivantavarez2@gmail.com>
2023-11-27 12:47:05 -0500

    Created Order class

--- src/main/java/com/pluralsight/Order.java
+++ src/main/java/com/pluralsight/Order.java
@@ -1,4 +1,25 @@
 package com.pluralsight;
+import java.util.List;
 
 public class Order {
+    private int orderID;
+    private String customerName;
+    private List<Sandwich> sandwiches;
+    private List<Drink> drinks;
+    private List<Chip> chips;
+
+    public Order(int orderID, String customerName, List<Sandwich> sandwiches, List<Drink> drinks, List<Chip> chips) {
+        this.orderID = orderID;
+        this.customerName = customerName;
+        this.sandwiches = sandwiches;
+        this.drinks = drinks;
+        this.chips = chips;
+    }
+
+    public double calculateTotalPrice() {
+        return 0;
+    }
+
+    public void displayOrderDetails() {
+    }
 }
