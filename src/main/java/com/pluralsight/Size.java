0245397d06ce9b220e206b4d5b4000ace608aa8b
Ivan Tavarez <ivantavarez2@gmail.com>
2023-11-27 12:46:50 -0500

    Created Size class

--- src/main/java/com/pluralsight/Size.java
+++ src/main/java/com/pluralsight/Size.java
@@ -1,4 +1,14 @@
 package com.pluralsight;
 
 public class Size {
+    private String sizeType;
+
+    public Size(String sizeType) {
+        this.sizeType = sizeType;
+    }
+
+    public String getSizeType() {
+        return sizeType;
+    }
 }
+
