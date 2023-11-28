67326de1d0c892efe276ffe8192bcb3fdd9ac65d
Sing Tuttanon <singtuttanon@gmail.com>
2023-11-28 11:21:56 -0500

    Updated README.md

--- src/main/java/com/pluralsight/RegularList.java
+++ src/main/java/com/pluralsight/RegularList.java
@@ -1,7 +1,7 @@
 package com.pluralsight;
 
 public class RegularList {
-    private String[] vegetables = {"lettuce", "peppers", "onions", "tomatoes", "jalepenos", "cucumbers", "pickles", "guacamole", "mushrooms"};
+    private String[] vegetables = {"lettuce", "peppers", "onions", "tomatoes", "jalapenos", "cucumbers", "pickles", "guacamole", "mushrooms"};
     private String[] sauces = {"mayo", "mustard", "ketchup", "ranch", "thousand islands", "vinaigrette"};
     private String[] sides = {"au jus", "sauce"};
 
