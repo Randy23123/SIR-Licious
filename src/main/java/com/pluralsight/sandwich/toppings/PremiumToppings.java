7d0c614f9c72ef863215cdd3dc97a4ae433c0f25
Sing Tuttanon <singtuttanon@gmail.com>
2023-11-30 11:46:56 -0500

    Merged Meat/Cheese to PremiumToppings (Slight modifcations: OrderMenu

--- src/main/java/com/pluralsight/sandwich/toppings/PremiumToppings.java
+++ src/main/java/com/pluralsight/sandwich/toppings/PremiumToppings.java
@@ -2,14 +2,19 @@ package com.pluralsight.sandwich.toppings;
 
 import com.pluralsight.sandwich.Bread;
 
-public abstract class PremiumToppings extends Bread implements Toppings {
-    private final String name;
+import java.util.ArrayList;
 
-    public PremiumToppings(String breadType, String sizeType, String name) {
+public class PremiumToppings extends Bread implements Toppings {
+    private final String name;
+    private double basePrice;
+    private double extraPrice;
+
+    public PremiumToppings(String name, String breadType, String sizeType, double basePrice, double extraPrice) {
         super(breadType, sizeType);
         this.name = name;
+        this.basePrice = basePrice;
+        this.extraPrice = extraPrice;
     }
-
 
     @Override
     public String getName() {
@@ -18,6 +23,77 @@ public abstract class PremiumToppings extends Bread implements Toppings {
 
     @Override
     public double getPrice() {
-        return 0;
+        if (extraPrice == 0) {
+            return basePrice;
+        } else {
+            return basePrice;
+        }
+    }
+
+    public static ArrayList<PremiumToppings> createMeatsList(String breadType, String sizeType) {
+        ArrayList<PremiumToppings> meatsList = new ArrayList<>();
+
+        switch (sizeType) {
+            case "4":
+                meatsList.add(new PremiumToppings("steak", breadType, sizeType, 1.0, 0.5));
+                meatsList.add(new PremiumToppings("ham", breadType, sizeType, 1.0, 0.5));
+                meatsList.add(new PremiumToppings("salami", breadType, sizeType, 1.0, 0.5));
+                meatsList.add(new PremiumToppings("roast beef", breadType, sizeType, 1.0, 0.5));
+                meatsList.add(new PremiumToppings("chicken", breadType, sizeType, 1.0, 0.5));
+                meatsList.add(new PremiumToppings("bacon", breadType, sizeType, 1.0, 0.5));
+                break;
+            case "8":
+                meatsList.add(new PremiumToppings("steak", breadType, sizeType, 2.0, 1.0));
+                meatsList.add(new PremiumToppings("ham", breadType, sizeType, 2.0, 1.0));
+                meatsList.add(new PremiumToppings("salami", breadType, sizeType, 2.0, 1.0));
+                meatsList.add(new PremiumToppings("roast beef", breadType, sizeType, 2.0, 1.0));
+                meatsList.add(new PremiumToppings("chicken", breadType, sizeType, 2.0, 1.0));
+                meatsList.add(new PremiumToppings("bacon", breadType, sizeType, 2.0, 1.0));
+                break;
+            case "12":
+                meatsList.add(new PremiumToppings("steak", breadType, sizeType, 3.0, 1.5));
+                meatsList.add(new PremiumToppings("ham", breadType, sizeType, 3.0, 1.5));
+                meatsList.add(new PremiumToppings("salami", breadType, sizeType, 3.0, 1.5));
+                meatsList.add(new PremiumToppings("roast beef", breadType, sizeType, 3.0, 1.5));
+                meatsList.add(new PremiumToppings("chicken", breadType, sizeType, 3.0, 1.5));
+                meatsList.add(new PremiumToppings("bacon", breadType, sizeType, 3.0, 1.5));
+                break;
+            default:
+                System.out.println("Error"); // can remove later
+                break;
+        }
+        return meatsList;
+    }
+
+    public static ArrayList<PremiumToppings> createCheeseList(String breadType) {
+        ArrayList<PremiumToppings> cheeseList = new ArrayList<>();
+
+        switch (sizeType) {
+            case "4":
+                cheeseList.add(new PremiumToppings("cheddar", breadType, sizeType, 0.75, 0.30));
+                cheeseList.add(new PremiumToppings("swiss", breadType, sizeType, 0.75, 0.30));
+                cheeseList.add(new PremiumToppings("pepper jack", breadType, sizeType, 0.75, 0.30));
+                cheeseList.add(new PremiumToppings("provolone", breadType, sizeType, 0.75, 0.30));
+                cheeseList.add(new PremiumToppings("american", breadType, sizeType, 0.75, 0.30));
+                break;
+            case "8":
+                cheeseList.add(new PremiumToppings("cheddar", breadType, sizeType, 1.50, 0.60));
+                cheeseList.add(new PremiumToppings("swiss", breadType, sizeType, 1.50, 0.60));
+                cheeseList.add(new PremiumToppings("pepper jack", breadType, sizeType, 1.50, 0.60));
+                cheeseList.add(new PremiumToppings("provolone", breadType, sizeType, 1.50, 0.60));
+                cheeseList.add(new PremiumToppings("american", breadType, sizeType, 1.50, 0.60));
+                break;
+            case "12":
+                cheeseList.add(new PremiumToppings("cheddar", breadType, sizeType, 2.25,0.90));
+                cheeseList.add(new PremiumToppings("swiss", breadType, sizeType, 2.25,0.90));
+                cheeseList.add(new PremiumToppings("pepper jack", breadType, sizeType, 2.25,0.90));
+                cheeseList.add(new PremiumToppings("provolone", breadType, sizeType, 2.25,0.90));
+                cheeseList.add(new PremiumToppings("american", breadType, sizeType, 2.25,0.90));
+                break;
+            default:
+                System.out.println("Error"); // can remove later
+                break;
+        }
+        return cheeseList;
     }
 }
